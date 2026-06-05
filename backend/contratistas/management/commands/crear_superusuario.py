481ee500d1e8552f2816c75d36d19db8b8a80ae8
Jorge Pablo <jorgepablo@MacBook-Air-de-Jorge.local>
2026-06-05 17:22:02 -0400

    agregar comando crear_superusuario para producción

---
+++ backend/contratistas/management/commands/crear_superusuario.py
@@ -0,0 +1,15 @@
+import os
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+        email = os.environ.get('DJANGO_SUPERUSER_EMAIL', '')
+        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', '')
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username, email, password)
+            self.stdout.write(f'Superusuario {username} creado.')
+        else:
+            self.stdout.write(f'Superusuario {username} ya existe.')
