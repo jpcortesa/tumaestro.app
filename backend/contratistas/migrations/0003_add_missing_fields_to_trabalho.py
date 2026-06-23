8b03228c9ed1610ed81193d76a905a53fc5f6e02
Jorge Pablo <jorgepablo@MacBook-Air-de-Jorge.local>
2026-06-23 19:35:35 -0400

    fix: empty 0003 migration - fields already exist in render db

---
+++ backend/contratistas/migrations/0003_add_missing_fields_to_trabalho.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('contratistas', '0002_create_missing_models'),
+    ]
+
+    operations = [
+    ]
